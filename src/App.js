ab1327d5c452bc85a9f595410ee99d50bea8d7a6
Baklay Petro <pustota1988@gmail.com>
2023-06-16 19:54:54 +0300

    createHeader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Main from './page/Main';
-import './App.css';
+
 import { Route, Routes } from 'react-router-dom';
+import Main from './page/Main';
+import Header from './components/Header';
+import './App.css';
 
 function App() {
     return (
         <div className="App">
+          <Header />
             <Routes>
               <Route path='/' element={<Main />} />
                 
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo.jpg'
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header-container">
+            <div className="logo">
+                <img src={logo} alt="Logo" className="logo-image" />
+            </div>
+            <nav className="nav-container">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/login" className="nav-link">
+                            Login
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/register" className="nav-link">
+                            Registration
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
