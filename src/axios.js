b90246a72d758fbcb1d9e5f75c36b3359c41c63c
Baklay Petro <pustota1988@gmail.com>
2023-06-28 23:31:08 +0300

    fix

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:80' || 'https://elated-deer-loincloth.cyclic.app/'
+    baseURL: 'https://elated-deer-loincloth.cyclic.app/'
 })
 
 instance.interceptors.request.use((config) => {
